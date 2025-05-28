610e2fb23e94e5236f27680c3cef2fc7560db77c
SHUBBIE07 <150241351+SHUBBIE07@users.noreply.github.com>
2025-05-28 15:58:26 +0100

    Add files via upload

---
+++ drum.js
@@ -0,0 +1,62 @@
+const drumButtons = document.querySelectorAll('.drum');
+
+drumButtons.forEach(button => {
+  button.addEventListener('click', function () {
+    const buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML)
+  });
+});
+
+document.addEventListener("keydown", function(event){
+  makeSound(event.key);
+  buttonAnimation(event.key);
+})
+
+function makeSound(key) {
+  switch (key) {
+    case 'w':
+      new Audio('sounds/tom-1.mp3').play();
+      break;
+
+    case 'a':
+      new Audio('sounds/tom-2.mp3').play();
+      break;
+
+    case 's':
+      new Audio('sounds/tom-3.mp3').play();
+      break;
+
+    case 'd':
+      new Audio('sounds/tom-4.mp3').play();
+      break;
+
+    case 'j':
+      new Audio('sounds/snare.mp3').play();
+      break;
+
+    case 'k':
+      new Audio('sounds/crash.mp3').play();
+      break;
+
+    case 'l':
+      new Audio('sounds/kick-bass.mp3').play();
+      break;
+
+    default:
+      console.log('Invalid key:', key);
+  }
+}
+
+
+function buttonAnimation(currentKey) {
+  const activeButton = document.querySelector(`.${currentKey}`)
+
+  activeButton.classList.add('pressed')
+
+  setTimeout(()=>{
+   activeButton.classList.remove('pressed')
+
+  },100)
+
+}
